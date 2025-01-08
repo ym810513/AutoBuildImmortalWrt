04034f7c0e7c811c981f3d061ff53fb0f12c016f
ym810513 <53586977+ym810513@users.noreply.github.com>
2025-01-07 18:06:11 -0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -35,7 +35,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by MingYUAN"
+NEW_DESCRIPTION="Compiled by MingYuan"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
